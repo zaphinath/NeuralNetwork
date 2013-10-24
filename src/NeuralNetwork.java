1e7467d78d6ed273fe08bb6b62d9770c300051bf
Derek Carr <zaphinath@gmail.com>
2013-10-24 10:26:00 -0600

    Cleaning up and implmented wight and sigmoid matrices

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -10,94 +10,93 @@ import java.util.ArrayList;
  */
 public class NeuralNetwork {
 	
+	
+	//Size Weight Matrix [
+	
+	
+	
+	private int maxEpochs = 4000;
+	private double learnRate = 0.01;
+	private double startWeight = .01;
+	
 	//Need to be set
-    private int maxEpochs = 4000;
-    private double learnRate = 0.01;
-    private int numInput; //Number of input nodes
+	private int numInput; //Number of input nodes
 	private int numHidden; //Number of hidden node layers
 	private int numOutput; //Number of output Nodes
+	private int[] numNodesPerHidden; //Number of nodes in each layer. Each index corresponds to each layer - 0 is first hidden layer
 	      
-	private double[] inputs;
-	 
-	private double[][] ihWeights; // input-hidden
-	private double[] hBiases;
-	private double[] hOutputs;
-	 
-	private double[][] hoWeights; // hidden-output
-	private double[] oBiases;
-	 
-	private double[] outputs;
-	 
-	// back-prop specific arrays (these could be local to method UpdateWeights)
-	private double[] oGrads; // output gradients for back-propagation
-	private double[] hGrads; // hidden gradients for back-propagation
-	 
-	// back-prop momentum specific arrays (these could be local to method Train)
-	//private double[][] ihPrevWeightsDelta;  // for momentum with back-propagation
-	private double[] hPrevBiasesDelta;
-	private double[][] hoPrevWeightsDelta;
-	private double[] oPrevBiasesDelta;
-	
-	
-	//Weights
-	//Sigmoid Function 1/(1+e^-(Input_all * Weight_all))
-	/**
-	 * Needs to add one to every layer except output for a bias
-	 * @param numInputs
-	 * @param numHiddenLayers
-	 * @param numHiddenNodesPerLayer
-	 * @param numberOutput
-	 */
-	public NeuralNetwork(int numInput, int numHidden, int numOutput) {
-		this.numInput = numInput; 
-		this.numHidden = numHidden; 
-		this.numOutput = numOutput; 
+	private double[][] weights; //[eachLayer][nodesperlayer]
+	private double[][] sigNodes;//[numhidden+1 (for outputlayer)][nodes per layer]
+
+	public NeuralNetwork(int numInput, int numHidden, int[] numNodesPerHidden, int numOutput) {
+		assert numNodesPerHidden.length == numHidden;
 		
-		this.inputs = new double[numInput];
+		this.numInput = numInput;
+		this.numHidden = numHidden;
+		this.numNodesPerHidden = numNodesPerHidden;
+		this.numOutput = numOutput;
 		
-		this.ihWeights = makeMatrix(numInput, numHidden);
-		this.hBiases = new double[numHidden];
-		this.hOutputs = new double[numHidden];
-		
-		this.hoWeights = makeMatrix(numHidden, numOutput);
-		this.oBiases = new double[numOutput];
-		
-		this.outputs = new double[numOutput];
-		
-		// back-prop related arrays below
-		this.hGrads = new double[numHidden];
-		this.oGrads = new double[numOutput];
-		
-		//this.ihPrevWeightsDelta = makeMatrix(numInput, numHidden);
-		this.hPrevBiasesDelta = new double[numHidden];
-		this.hoPrevWeightsDelta = makeMatrix(numHidden, numOutput);
-		this.oPrevBiasesDelta = new double[numOutput];
+		this.weights = createWeights(numInput, numHidden, numNodesPerHidden, numOutput);
+		this.sigNodes = createSigmoidMatrix(numHidden, numNodesPerHidden,numOutput);
 		
 	}
 
-	public void train() {
+	public void train(double[] inputValues, int expectedNode) {
 		
 	}
 	
 	public void test() {
 		
 	}
-	
-	private double[][] makeMatrix(int rows, int cols) {
-		double[][] result = new double[rows][];
-        for (int r = 0; r < result.length; ++r) {        	
-        	result[r] = new double[cols];
-        }
-        return result;
+	/**
+	 * Creates a 2D Matrix of varying heights that represents the weight between layers
+	 * and initializes the starting weights.
+	 * @param numInput2
+	 * @param numHidden2
+	 * @param numNodesPerHidden2
+	 * @param numOutput2
+	 * @return
+	 */
+	private double[][] createWeights(int numInput,
+			int numHidden, int[] numNodesPerHidden, int numOutput) {
+		double[][] w = new double[numHidden + 1][];
+		//Set num weights from input nodes
+		w[0] = new double[numInput*numNodesPerHidden[0]];
+		for (int i = 0; i < w[0].length; i++) {
+			w[0][i] = startWeight;
+		}
+		//set output of each hidden
+		for (int i = 1; i < numHidden; i++) {
+			w[i] = new double[numNodesPerHidden[i-1]*numNodesPerHidden[i]];
+			for (int j = 0; j < w[i].length; j++) {
+				w[i][j] = startWeight;
+			}
+		}
+		//set last weight between last hidden and output
+		w[numHidden] = new double[numNodesPerHidden[numHidden-1]*numOutput];
+		for (int i = 0; i < w[numHidden].length; i++) {
+			w[numHidden][i] = startWeight;
+		}		
+		return w;
 	}
+
 	
-	private void initWeights() {
-		int numWeights = (numInput * numHidden) + (numHidden * numOutput) + numHidden + numOutput;
-        double[] initWeights = new double[numWeights];
-        double lo = -0.01;
-        double hi = 0.01;
-        for (int i=0; i < initWeights.length ; ++i)
-                initWeights[i] = (hi - lo) * rnd.nextDouble() + lo;
-        this.setWeights(initWeights);
+	/**
+	 * @param numHidden2
+	 * @param numNodesPerHidden2
+	 * @param numOutput2
+	 * @return
+	 */
+	private double[][] createSigmoidMatrix(int numHidden,
+			int[] numNodesPerHidden, int numOutput) {
+		double[][] sig = new double[numHidden+1][];
+		//create hidden layers
+		for (int i = 0; i < sig.length; i++) {
+			sig[i] = new double[numNodesPerHidden[i]];
+		}
+		//create output sigmoid layer
+		sig[numHidden+1] = new double[numOutput];
+		return sig;
 	}
+
 }
