610a1282106c7ae671cf29e465cb0926c6dc72f0
Derek Carr <zaphinath@gmail.com>
2013-10-24 13:18:58 -0600

    Feed Forward working with sigmoid functions
    Need to back propogate for one epoch now

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -10,14 +10,9 @@ import java.util.ArrayList;
  */
 public class NeuralNetwork {
 	
-	
-	//Size Weight Matrix [
-	
-	
-	
 	private int maxEpochs = 4000;
-	private double learnRate = 0.01;
-	private double startWeight = .01;
+	private double learnRate = 0.3;
+	private double startWeight = .1;
 	
 	//Need to be set
 	private int numInput; //Number of input nodes
@@ -27,6 +22,7 @@ public class NeuralNetwork {
 	      
 	private double[][] weights; //[eachLayer][nodesperlayer]
 	private double[][] sigNodes;//[numhidden+1 (for outputlayer)][nodes per layer]
+	private double[][] tmpCalcMatrix;//Same size as weights for calculating input*weight before sigmoid is calculated and put in sigNodes
 
 	public NeuralNetwork(int numInput, int numHidden, int[] numNodesPerHidden, int numOutput) {
 		assert numNodesPerHidden.length == numHidden;
@@ -38,16 +34,202 @@ public class NeuralNetwork {
 		
 		this.weights = createWeights(numInput, numHidden, numNodesPerHidden, numOutput);
 		this.sigNodes = createSigmoidMatrix(numHidden, numNodesPerHidden,numOutput);
-		
+		//this.tmpCalcMatrix = cloneEmptyMatrix(weights);
 	}
 
-	public void train(double[] inputValues, int expectedNode) {
+
+	public void train(double[] inputValues, int expectedOutNode) {
+		//calculate sigmoids
+		for (int i = 0; i < sigNodes.length; i++) {
+			for (int j = 0; j < sigNodes[i].length; j++) {
+				// use input values to start with
+				double tmpSum = 0;
+				if (i == 0) {
+					for (int k = 0; k < weights[0].length; k++) {
+						int tmpCount = 0; // this is to match the input node position 
+						if (k % inputValues.length == j) {
+							tmpSum += inputValues[tmpCount]*weights[0][k];
+							tmpCount++;
+						}
+					}
+				} else {
+					for (int k = 0; k < weights[i].length; k++) {
+						int tmpCount = 0; // this is to match the input node position 
+						if (k % sigNodes[i].length == j) {
+							tmpSum += sigNodes[i-1][tmpCount]*weights[i][k];
+							tmpCount++;
+						}
+					}
+				}
+				sigNodes[i][j] = 1/(1+Math.pow(Math.E,-(tmpSum)));
+			}
+		}
+	}
+	
+	public void test(double[] inputValues, int expectedOutNode) {
 		
 	}
 	
-	public void test() {
-		
+	/**
+	 * @return the maxEpochs
+	 */
+	public int getMaxEpochs() {
+		return maxEpochs;
 	}
+
+
+	/**
+	 * @param maxEpochs the maxEpochs to set
+	 */
+	public void setMaxEpochs(int maxEpochs) {
+		this.maxEpochs = maxEpochs;
+	}
+
+
+	/**
+	 * @return the learnRate
+	 */
+	public double getLearnRate() {
+		return learnRate;
+	}
+
+
+	/**
+	 * @param learnRate the learnRate to set
+	 */
+	public void setLearnRate(double learnRate) {
+		this.learnRate = learnRate;
+	}
+
+
+	/**
+	 * @return the startWeight
+	 */
+	public double getStartWeight() {
+		return startWeight;
+	}
+
+
+	/**
+	 * @param startWeight the startWeight to set
+	 */
+	public void setStartWeight(double startWeight) {
+		this.startWeight = startWeight;
+	}
+
+
+	/**
+	 * @return the numInput
+	 */
+	public int getNumInput() {
+		return numInput;
+	}
+
+
+	/**
+	 * @param numInput the numInput to set
+	 */
+	public void setNumInput(int numInput) {
+		this.numInput = numInput;
+	}
+
+
+	/**
+	 * @return the numHidden
+	 */
+	public int getNumHidden() {
+		return numHidden;
+	}
+
+
+	/**
+	 * @param numHidden the numHidden to set
+	 */
+	public void setNumHidden(int numHidden) {
+		this.numHidden = numHidden;
+	}
+
+
+	/**
+	 * @return the numOutput
+	 */
+	public int getNumOutput() {
+		return numOutput;
+	}
+
+
+	/**
+	 * @param numOutput the numOutput to set
+	 */
+	public void setNumOutput(int numOutput) {
+		this.numOutput = numOutput;
+	}
+
+
+	/**
+	 * @return the numNodesPerHidden
+	 */
+	public int[] getNumNodesPerHidden() {
+		return numNodesPerHidden;
+	}
+
+
+	/**
+	 * @param numNodesPerHidden the numNodesPerHidden to set
+	 */
+	public void setNumNodesPerHidden(int[] numNodesPerHidden) {
+		this.numNodesPerHidden = numNodesPerHidden;
+	}
+
+
+	/**
+	 * @return the weights
+	 */
+	public double[][] getWeights() {
+		return weights;
+	}
+
+
+	/**
+	 * @param weights the weights to set
+	 */
+	public void setWeights(double[][] weights) {
+		this.weights = weights;
+	}
+
+
+	/**
+	 * @return the sigNodes
+	 */
+	public double[][] getSigNodes() {
+		return sigNodes;
+	}
+
+
+	/**
+	 * @param sigNodes the sigNodes to set
+	 */
+	public void setSigNodes(double[][] sigNodes) {
+		this.sigNodes = sigNodes;
+	}
+
+
+	/**
+	 * @return the tmpCalcMatrix
+	 */
+	public double[][] getTmpCalcMatrix() {
+		return tmpCalcMatrix;
+	}
+
+
+	/**
+	 * @param tmpCalcMatrix the tmpCalcMatrix to set
+	 */
+	public void setTmpCalcMatrix(double[][] tmpCalcMatrix) {
+		this.tmpCalcMatrix = tmpCalcMatrix;
+	}
+
+
 	/**
 	 * Creates a 2D Matrix of varying heights that represents the weight between layers
 	 * and initializes the starting weights.
@@ -91,12 +273,31 @@ public class NeuralNetwork {
 			int[] numNodesPerHidden, int numOutput) {
 		double[][] sig = new double[numHidden+1][];
 		//create hidden layers
-		for (int i = 0; i < sig.length; i++) {
+		for (int i = 0; i < numHidden; i++) {
 			sig[i] = new double[numNodesPerHidden[i]];
 		}
 		//create output sigmoid layer
-		sig[numHidden+1] = new double[numOutput];
+		sig[numHidden] = new double[numOutput];
 		return sig;
 	}
+	
+//	/**
+//	 * @param weights
+//	 * @return
+//	 */
+//	private double[][] cloneEmptyMatrix(double[][] weights) {
+//		double[][] clone = new double[weights.length][];
+//		for (int i = 0; i < weights[0].length; i++) {
+//			clone[i] = new double[weights[i].length];
+//		}
+//		return null;
+//	}
 
+	public void printMatrix(double[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.println("i=" + i + "  j=" + j + "  value="+matrix[i][j]);
+			}
+		}
+	}
 }
